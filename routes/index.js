272ff73e3a080a356b9a240ecec442289e044336
Duy <duyychiha@gmail.com>
2017-12-29 20:06:59 +0700

    Forgotpassword completed

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var passport = require('passport');
+
 var router = express.Router();
 var User = require('../model/user');
-
+var bodyParser = require('body-parser');
+var urlencodedParser=bodyParser.urlencoded({extended:false});
 var mongoose= require('mongoose');
+const nodemailer = require('nodemailer');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/user/signin');
@@ -30,6 +34,52 @@ router.get('/active/:id',function (req,res) {
 
     });
 });
+
+router.get('/forgotpwd', function(req,res,next){
+    var messages = req.flash('error');
+    res.render('user/forgotpwd');
+});
+router.post('/forgotpwd',urlencodedParser,function (req,res) {
+
+    var M = req.body.email;
+    console.log(M);
+    User.findOne({'email': M}, function (err, user) {
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, {message: 'User not found!'});
+        }
+        var p = Math.floor(Math.random() * 899999) + 100000;
+        var new_user = user;
+        new_user.password = new_user.encryptPassword(p);
+        user.update(new_user, function (error) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+
+            var mailOptions = {
+                from: 'KCoin shop <duyychiha9@gmail.com>',
+                to: M, // list of receivers
+                subject: 'Forgotpassword', // Subject line
+                text: 'Your new-password ' + p// plain text body
+
+            };
+
+
+            email_transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    //  console.log(error);
+                }
+            });
+            res.redirect('user/signin');
+        });
+    });
+});
+
+
 /*router.get('/user/signup', function(req,res,next){
 	var messages = req.flash('error');
 	res.render('user/signup',{csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length >0});
