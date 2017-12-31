0a294031480e0155ac0d6c6b5858fc7c54430d9e
anhky2004 <anhky2004@gmail.com>
2017-12-31 12:09:38 +0700

    hien thong bao da active thanh cong user

--- model/user.js
+++ model/user.js
@@ -3,11 +3,14 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
 
+
 var userSchema = new mongoose.Schema(
     {
+        name:{type: String, required:true},
         email:{type: String, required:true},
-  		password:{type:String, required:true}
-       
+  		password:{type:String, required:true},
+        is_active:{type:Boolean, required:true},
+        transaction_id:{type:Schema.Types.ObjectId, required:true}
 });
 
 userSchema.methods.encryptPassword = function(password){
@@ -17,6 +20,10 @@ userSchema.methods.encryptPassword = function(password){
 userSchema.methods.validPassword = function(password){
     return bcrypt.compareSync(password, this.password);
 };
-
+//var User = module.exports = mongoose.model('User', userSchema, 'user');
+var User = mongoose.model('User',userSchema);
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+};
 /*mongoose.model('User', StudentSchema);*/
 module.exports = mongoose.model('User',userSchema);
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
+var mongoose = require('mongoose');
+var User = require('../model/user');
+var Transaction = require('../model/transaction');
+
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
@@ -14,22 +18,29 @@ router.get('/logout',isLoggedIn, function(req, res,next){
   res.redirect('/');
 });
 
-router.use('/', notLoggedIn, function(req, res, next){
-    next();
+router.get('/dashboard',isLoggedIn, function(req, res,next){
+  var id = req.user.transaction_id;
+  var user_usable_balance = 0;
+  var user_current_balance = 0;
+  var transaction = Transaction.findById(req.user.transaction_id,function(err,data){
+    user_usable_balance = data.usable_balance;
+    user_current_balance = data.real_balance;
+    res.render('dashboard/dashboard',{ layout: false,username:req.user.name,usable_balance: user_usable_balance,current_balance: user_current_balance,transaction_address : data.address });
+  });
 });
 
-/* GET users listing. */
-/*router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});*/
 
 router.get('/signup', function(req,res,next){
 	var messages = req.flash('error');
-	res.render('user/signup',{csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length >0});
+ 
+  /*  res.render('index', { messages: req.flash('info') });*/
+	res.render('user/signup',{csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length >0 });
 });
 
+
 router.post('/signup',passport.authenticate('local.signup',{
-		successRedirect:  '/user/profile',
+
+		successRedirect:  '/user/signin',
 		failureRedirect:   '/user/signup',
 		failureFlash: true
 }));
@@ -38,33 +49,21 @@ router.post('/signup',passport.authenticate('local.signup',{
 
 router.get('/signin',function(req,res,next){
 	var messages = req.flash('error');
-	res.render('user/signin',{csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length >0});
+  var index = req.flash('info');
+	res.render('user/signin',{csrfToken: req.csrfToken(), messages: messages, index: index, hasErrors: messages.length >0});
 });
 
 router.post('/signin',passport.authenticate('local.signin',{
-		successRedirect:  '/user/profile',
-		failureRedirect:   '/user//signin',
+		successRedirect:  '/user/dashboard',
+		failureRedirect:   '/user/signin',
 		failureFlash: true
 }));
-
-
-
-module.exports = router;
 
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
   }
-  res.redirect('/');
+  res.redirect('/user/signin');
 }
-
-function notLoggedIn(req, res, next){
-  if(!req.isAuthenticated()){
-    return next();
-  }
-  res.redirect('/');
-}
-
-
 
 module.exports = router;
